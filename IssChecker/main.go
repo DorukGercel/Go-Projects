b81650143b79f42438a1c588cc5fd9dfd633dfb9
doruk.gercel <doruk.gercel@metu.edu.tr>
2021-11-08 16:37:26 +0300

    Implement go routines and channel

--- IssChecker/main.go
+++ IssChecker/main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/DorukGercel/Go-Projects/IssChecker/services/crawler"
+	"time"
 )
 
 func main() {
 	var s crawler.Satellite
-	d, err := crawler.SendGetRequest(s)
-	if err != nil {
-		return
+	channel := make(chan []crawler.Data)
+	for {
+		go func(c chan []crawler.Data) {
+			fmt.Println("Satellite request send...")
+			crawler.SendGetRequest(s, c)
+		}(channel)
+		crawler.PrintInfo(<-channel)
+		// Send new request after 5 seconds
+		time.Sleep(5 * time.Second)
 	}
-	d.PrintInfo()
 }
