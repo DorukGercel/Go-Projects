ee2e49ae12fb50b2227dedff9773ae22caf0c426
DorukGercel <dorukgercel@hotmail.com>
2022-01-17 22:55:51 +0300

    [TODOCli]: create message definitions

---
+++ TODO CLI/definitions/error_messages.go
@@ -0,0 +1,11 @@
+package definitions
+
+// Defined error messages
+const (
+	ErrAc    = "already complete"
+	ErrRde   = "record does not exist"
+	ErrNarg  = "not an appropriate argument"
+	ErrNfp   = "no flag was passed"
+	ErrNsf   = "no service found"
+	ErrInter = "internal error occurred"
+)
---
+++ TODO CLI/definitions/success_messages.go
@@ -0,0 +1,8 @@
+package definitions
+
+// Defined success messages
+const (
+	SucAdd    = "item added"
+	SucMark   = "item completed"
+	SucDelete = "item deleted"
+)
